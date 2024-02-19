2138045781b0114dbb749ae7a9f4a7414b067b20
Uday Mahajan <udaymahajan512@gmail.com>
2024-02-18 22:36:19 -0800

    First Commit - Initialized Repository Caliber

---
+++ src/main/java/dev/uday/caliber/CaliberApplication.java
@@ -0,0 +1,14 @@
+package dev.uday.caliber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliberApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/dev/uday/caliber/CaliberApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.uday.caliber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
